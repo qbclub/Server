0dac97b589cd4c78a6a72e4faf7eb7596c3738e1
grachevrv <grachevrv@ya.ru>
2022-02-22 18:55:43 +0400

    Connect mongo

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ const express = require('express')
 
 const app = express()
 const port = 3000;
+var cors = require('cors')
+
+let mongoClient = require('mongodb').MongoClient
+
+const {
+    url
+} = require('./password');
+const {
+    response
+} = require('express');
+
+let books, bookflow, users
+mongoClient.connect(url, function (err, client) {
+    let database = client.db("libraryFromNode")
+
+    books = database.collection("books")
+    bookflow = database.collection("bookflow")
+    users = database.collection("users")
+
+    console.log('mongo connected')
+})
+
+
+app.use(cors())
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
 
 // Запрос с адреса: http://localhost:3000/
 app.get('/', function(request, response) {
