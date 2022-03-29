c1a2439a967c3608e6edd6029f7f6b86b850757c
VChernyaev <chernyaevolodya@gmail.com>
2022-03-29 18:53:27 +0400

    import collection from mongo

---
+++ middlewares/getbooksbyId.js
@@ -0,0 +1,15 @@
+const {getBooks} = require('../mongo')
+
+function getBookById(request, response) {
+    // const books = getBookById()
+    // let r  = books.find({}).toArray(function(err, documents){
+    //     response.send(JSON.stringify(documents))
+    // });
+    
+    // console.log(r)
+    // console.log(books)
+}
+
+module.exports = {
+    getBookById
+}
