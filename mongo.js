b323102aa54db9fc6f42e6e98462364b21a8f18a
VChernyaev <chernyaevolodya@gmail.com>
2022-04-19 18:29:51 +0400

    getUsers

--- mongo.js
+++ mongo.js
@@ -4,16 +4,24 @@ const {
 } = require('./password');
 
 let books;
+let users;
 mongoClient.connect(url, function (err, client) {
     let database = client.db("libraryFromNode")
     
     books = database.collection('books')
+    users = database.collection('users')
 })
 
 function getBooks() {
+
     return books
 }
+function getUsers() {
+    return users
+}
+
 
 module.exports = {
-    getBooks
+    getBooks,
+    getUsers
 }
