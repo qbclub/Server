c97fae38e4de506f11a02a3dbc02f2335906de84
VChernyaev <chernyaevolodya@gmail.com>
2022-04-19 18:38:27 +0400

    emailCollection

---
+++ middlewares/emailCollection.js
@@ -0,0 +1,13 @@
+const { getUsers } = require('../mongo')
+
+function getUsersByEmail(request, response) {
+    let users = getUsers()
+
+    users.find({ "Email": { $eq: "grishadzyin@gmail.com" } }).toArray(function(err, documents){
+        response.send(JSON.stringify(documents))
+    });
+}
+
+module.exports = {
+    getUsersByEmail
+}
